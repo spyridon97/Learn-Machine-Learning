5e941d3e70621eb0c184b1de4fafa32ad5885056
Spyridon Tsalikis <spyridon97@hotmail.com>
2019-11-28 16:10:04 -0500

    fix read_dataset

--- utils/io/read_dataset.py
+++ utils/io/read_dataset.py
@@ -1,27 +1,18 @@
-import csv
+import pandas
 
 
-def read_dataset(filename, delimiter="\t", headers=False, only_numbers=True):
+def read_dataset(filename, delimiter=",", header=False):
     """
     :brief: This function reads a file that represents a dataset.
 
     :param filename: is the name of the file
-    :param delimiter: is the delimiter that is used in the file, Default = "\t"
-    :param headers: is a boolean value which indicates if the file includes headers. Default = False
-    :param only_numbers: is a boolean value which indicates if the file includes only numbers. Default = True
+    :param delimiter: is the delimiter that is used in the file, Default = ","
+    :param header: is a boolean value which indicates if the file includes headers. Default = False
     :return: the dataset
     """
 
-    if not headers:
-        reader = csv.reader(open(filename), delimiter=delimiter)
-        if only_numbers:
-            dataset = [[float(number) for number in row[0].split()] for row in reader]
-        else:
-            dataset = [[]]
+    if not header:
+        dataset = pandas.read_csv(filename, delimiter=delimiter, header=None).values.tolist()
     else:
-        if only_numbers:
-            dataset = [[]]
-        else:
-            dataset = [[]]
-
+        dataset = pandas.read_csv(filename, delimiter=delimiter).values.tolist()
     return dataset
