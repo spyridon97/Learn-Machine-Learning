4dd77cce330f2728940be6af0ef08cd081b25575
Spyridon Tsalikis <spyridon97@hotmail.com>
2019-11-28 16:16:33 -0500

    fix read_dataset headers

--- utils/io/read_dataset.py
+++ utils/io/read_dataset.py
@@ -8,11 +8,14 @@ def read_dataset(filename, delimiter=",", header=False):
     :param filename: is the name of the file
     :param delimiter: is the delimiter that is used in the file, Default = ","
     :param header: is a boolean value which indicates if the file includes headers. Default = False
-    :return: the dataset
+    :return: the dataset, headers
     """
 
     if not header:
         dataset = pandas.read_csv(filename, delimiter=delimiter, header=None).values.tolist()
+        return dataset
     else:
-        dataset = pandas.read_csv(filename, delimiter=delimiter).values.tolist()
-    return dataset
+        dataset = pandas.read_csv(filename, delimiter=delimiter)
+        headers = dataset.columns.values.tolist()
+        dataset = dataset.values.tolist()
+        return dataset, headers
